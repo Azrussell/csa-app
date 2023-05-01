f75b71f3785eacbb6fb537f8b87e19a3f65d1f18
Aaliyah Russell <aaliyah_russell@yahoo.com>
2023-05-01 14:27:14 -0400

    username changes

--- config/config.js
+++ config/config.js
@@ -16,7 +16,7 @@ module.exports = {
     "dialect": "postgres"
   },
   "production": {
-    "username": process.env.DATABASE_USER,
+    "username": process.env.DATABASE_USERNAME,
     "password": process.env.DATABASE_PASSWORD,
     "database": process.env.DATABASE_NAME,
     "host": process.env.DATABASE_HOST,
